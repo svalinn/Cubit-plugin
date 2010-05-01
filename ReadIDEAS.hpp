bd8a9a98b28fe7b273da88ef595cfc3c63cba4d9
Brandon Smith <fathom@mcs.anl.gov>
2010-05-01 05:53:35 +0000

    • Previous version only supported tet elements. Added support for tri, quad, hex, and prism elements. • Documented sources of information about this format. • Elements are added to material sets instead of individually being tagged. • Added sample file with unit tests.
    git-svn-id: https://svn.mcs.anl.gov/repos/ITAPS/MOAB/trunk@3828 6162379e-bd28-0410-9a7d-b7f4dcfcad3c

--- ReadIDEAS.hpp
+++ ReadIDEAS.hpp
@@ -1,12 +1,17 @@
+#ifndef READIDEAS_HPP
+#define READIDEAS_HPP
+                  
+#ifndef IS_BUILDING_MB
+  #error "ReadIDEAS.hpp isn't supposed to be included into an application"
+#endif
+
 #include <iostream>
 #include <fstream>
 #include <vector>
 
 #include "moab/ReaderIface.hpp"
 #include "moab/Interface.hpp"
-
-#define VERTEX_LIST       2411 
-#define MAKE_TETRAHEDRA   2412
+#include "moab/RangeMap.hpp"
 
 #define MAT_PROP_TABLE_TAG "mat_prop_table"
 #define PHYS_PROP_TABLE_TAG "phys_prop_table"
@@ -46,11 +51,12 @@ protected:
   
   ErrorCode skip_header();
   ErrorCode create_vertices(EntityHandle& first_vertex, const Tag* file_id_tag);
-  ErrorCode create_tetrahedral_elements(EntityHandle first_vertex, const Tag* file_id_tag);
+  ErrorCode create_elements(EntityHandle first_vertex, const Tag* file_id_tag);
   
 private:
   
   std::ifstream file;
+  RangeMap<int, EntityHandle> nodeIdMap;
   
   // Read mesh interface
   ReadUtilIface* readMeshIface;
@@ -58,6 +64,54 @@ private:
   // MOAB Interface
   Interface* MBI;
 
+
+  /* Universal dataset numbers
+     An integer describes a chunk of information. These chunks include headers,
+     units, nodes, elements, patches, etc... described in the OpenFOAM IDEAS 
+     reader.
+
+     1) http://amira.zib.de/usersguide31/hxideas/HxFileFormat_IDEAS.html
+     55,2414 data at nodes
+
+     2) http://www.sdrl.uc.edu/universal-file-formats-for-modal-analysis-testing-1
+     /file-format-storehouse/unv_0015.htm/
+     15 nodes with single precision coordinates
+     line1 (4I10,1P3E13.5): node_label coord_sys_num displacement_sys_num 
+     color x y z */
+  static const unsigned SINGLE_PRECISION_NODES = 15;
+
+  /* 3) http://www.sdrl.uc.edu/pdf/test_universal_file_formats.pdf
+     781,2411 nodes with double precision coordinates
+     line1 (4I10):       node_label coord_sys_num displacement_sys_num color
+     line2 (1P3D25.16):  x y z */
+  static const unsigned DOUBLE_PRECISION_NODES0 = 781;
+  static const unsigned DOUBLE_PRECISION_NODES1 = 2411;
+
+  /* 4) http://www.sdrl.uc.edu/universal-file-formats-for-modal-analysis-testing-1
+     /file-format-storehouse/unv_0780.htm/
+     71, 780, 2412 element definitions
+     line1 (8I10): element_label fe_id phys_prop_bin_num phys_prop_num 
+     mat_prop_bin_num mat_prop_num color num_of_nodes
+     line2 (8I10): connectivity_node_labels */
+  static const unsigned ELEMENTS0 = 71;
+  static const unsigned ELEMENTS1 = 780;
+  static const unsigned ELEMENTS2 = 2412;
+
+  /* Mesh elements exist inside chunks 71, 780, and 2412. Each element definition 
+     includes the finite element id that describes the element type. These are
+     used in the OpenFOAM IDEAS reader. The canonical node ordering matches that
+     of MBCN, as suggested by the Gmsh 2.2.3 source code.*/
+  static const int ROD0  = 11;
+  static const int ROD1  = 171;
+  static const int TRI0  = 41;
+  static const int TRI1  = 91;
+  static const int QUAD0 = 44;
+  static const int QUAD1 = 94;
+  static const int TET   = 111;
+  static const int WEDGE = 112;     
+  static const int HEX   = 115;
+
 };
 
 } // namespace moab
+#endif
