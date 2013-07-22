ef5ff17eb33518add86e27f56cfee1ba75e11713
Dan Wu <wuda@mcs.anl.gov>
2013-07-22 10:08:50 -0500

    [Logic update] In case time variable is not available, create dummy time coordinate values instead of returning an error code.

--- NCHelperFV.cpp
+++ NCHelperFV.cpp
@@ -143,10 +143,11 @@ ErrorCode NCHelperFV::init_mesh_vals(const FileOptions& opts, EntityHandle file_
   gCDims[1] = 0;
   jCName = dimNames[idx];
 
+  // Look for time dimension
   if ((vit = std::find(dimNames.begin(), dimNames.end(), "time")) != dimNames.end())
     idx = vit - dimNames.begin();
   else {
-    ERRORR(MB_FAILURE, "Couldn't find time variable.");
+    ERRORR(MB_FAILURE, "Couldn't find time dimension.");
   }
   tDim = idx;
   tMax = dimVals[idx] - 1;
@@ -313,13 +314,16 @@ ErrorCode NCHelperFV::init_mesh_vals(const FileOptions& opts, EntityHandle file_
     }
   }
 
+  // Store time coordinate values in tVals
   if (tMin != -1) {
     if ((vmit = varInfo.find(tName)) != varInfo.end() && (*vmit).second.varDims.size() == 1) {
       rval = _readNC->read_coordinate(tName.c_str(), tMin, tMax, tVals);
       ERRORR(rval, "Trouble reading time variable.");
     }
     else {
-      ERRORR(MB_FAILURE, "Couldn't find time coordinate.");
+      // If expected time variable is not available, set dummy time coordinate values to tVals
+      for (int t = tMin; t <= tMax; t++)
+        tVals.push_back((double)t);
     }
   }
 
