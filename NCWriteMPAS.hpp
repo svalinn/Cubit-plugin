1096e78aec8bcadb3fa8e5def53b8a67ff965eff
Dan Wu <wuda@mcs.anl.gov>
2014-04-29 17:05:41 -0500

    Updated NC writer and its unit test. Serial and parallel write should work now for MPAS.

--- NCWriteMPAS.hpp
+++ NCWriteMPAS.hpp
@@ -17,7 +17,7 @@ class NCWriteMPAS: public UcdNCWriteHelper
 {
 public:
   NCWriteMPAS(WriteNC* writeNC, int fileId, const FileOptions& opts, EntityHandle fileSet)
-: UcdNCWriteHelper(writeNC, fileId, opts, fileSet) {}
+: UcdNCWriteHelper(writeNC, fileId, opts, fileSet), noEdges(false) {}
 
   virtual ~NCWriteMPAS();
 
@@ -26,7 +26,13 @@ private:
   virtual ErrorCode collect_mesh_info();
 
   //! Collect data for specified variables
+  virtual ErrorCode collect_variable_data(std::vector<std::string>& var_names);
+
+  //! Implementation of NCWriteHelper::write_values()
   virtual ErrorCode write_values(std::vector<std::string>& var_names);
+
+private:
+  bool noEdges;
 };
 
 } // namespace moab
