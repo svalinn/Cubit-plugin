ddf240c0e8380644f63885e136f654a70a66a449
Paul P.H. Wilson <paul.wilson@wisc.edu>
2017-09-14 14:52:26 -0500

    correct install script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-ln -s plugins/svalinn/libsvalinn_plugin.so plugins/.
+ln -s svalinn/libsvalinn_plugin.so plugins/.
 ln -s plugins/svalinn/libiGeom.so .
 ln -s plugins/svalinn/libmcnp2cad.so .
-ln -s plugins/svalinn/libMOAB.so .
+ln -s plugins/svalinn/libMOAB.so.0 .
 ln -s plugins/svalinn/libmakeWatertight.so .
