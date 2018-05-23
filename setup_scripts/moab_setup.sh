97fe96b05b0f9eb6263af27f3ee39916850fd7e4
Samuel Stern <sterns697@gmail.com>
2018-05-23 17:49:43 +0000

    Separated (sudo-dependent) apt call; Added style improvements

--- setup_scripts/moab_setup.sh
+++ setup_scripts/moab_setup.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
  
 #MOAB Installation
-INSTALL_ROUTE=$HOME
-cd $INSTALL_ROUTE
-
-apt install -y autoconf libtool make mpich libblas-dev liblapack-dev libhdf5-dev cmake
-
+INSTALL_ROOT=$HOME
+cd $INSTALL_ROOT
 mkdir MOAB
 cd MOAB
 mkdir bld
@@ -15,11 +12,11 @@ cd moab
 git checkout Version5.0
 autoreconf -fi
 cd ../bld
-../moab/configure --prefix=$INSTALL_ROUTE/MOAB/install --with-hdf5=/usr/lib/x86_64-linux-gnu/hdf5/serial --enable-shared;
+../moab/configure --prefix=$INSTALL_ROOT/MOAB/install --with-hdf5=/usr/lib/x86_64-linux-gnu/hdf5/serial --enable-shared
 make -j4
 make check
 make install
-printf 'export PATH=$PATH:%s/MOAB/install/bin' "$INSTALL_ROUTE">>$HOME/.bashrc
-export PATH=$PATH:$INSTALL_ROUTE/MOAB/install/bin
-printf 'export LD_LIBRARY_PATH=%s/MOAB/install/lib' "$INSTALL_ROUTE">>$HOME/.bashrc
-export LD_LIBRARY_PATH=$INSTALL_ROUTE/MOAB/install/lib
+printf 'export PATH=$PATH:$INSTALL_ROOT/MOAB/install/bin'>>$HOME/.bashrc
+export PATH=$PATH:$INSTALL_ROOT/MOAB/install/bin
+printf 'export LD_LIBRARY_PATH=$INSTALL_ROOT/MOAB/install/lib'>>$HOME/.bashrc
+export LD_LIBRARY_PATH=$INSTALL_ROOT/MOAB/install/lib
