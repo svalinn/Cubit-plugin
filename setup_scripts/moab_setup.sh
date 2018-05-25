a8f2e38d48e47ecacd489ecc0caf793d3662aa60
Samuel Stern <sterns697@gmail.com>
2018-05-25 21:09:36 +0000

    Fix /root/MOAB/install/bin:/root/MOAB/install/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin handling, added MOAB_INSTALL_DIR variable for consistency

--- setup_scripts/moab_setup.sh
+++ setup_scripts/moab_setup.sh
@@ -6,17 +6,19 @@ cd $INSTALL_ROOT
 mkdir MOAB
 cd MOAB
 mkdir bld
-mkdir install
+MOAB_INSTALL_DIR=$INSTALL_ROOT/MOAB/install
+mkdir $MOAB_INSTALL_DIR
 git clone https://bitbucket.org/fathomteam/moab
 cd moab
 git checkout Version5.0
 autoreconf -fi
 cd ../bld
-../moab/configure --prefix=$INSTALL_ROOT/MOAB/install --with-hdf5=/usr/lib/x86_64-linux-gnu/hdf5/serial --enable-shared
+../moab/configure --prefix=$MOAB_INSTALL_DIR --with-hdf5=/usr/lib/x86_64-linux-gnu/hdf5/serial --enable-shared
 make -j4
 make check
 make install
-printf 'export PATH=$PATH:$INSTALL_ROOT/MOAB/install/bin'>>$HOME/.bashrc
-export PATH=$PATH:$INSTALL_ROOT/MOAB/install/bin
-printf 'export LD_LIBRARY_PATH=$INSTALL_ROOT/MOAB/install/lib'>>$HOME/.bashrc
-export LD_LIBRARY_PATH=$INSTALL_ROOT/MOAB/install/lib
+printf 'Consider adding the following lines to .bashrc:\n'
+printf 'export PATH=$MOAB_INSTALL_DIR/bin:$PATH\n'
+export PATH=$MOAB_INSTALL_DIR/bin:$PATH
+printf 'export LD_LIBRARY_PATH=$MOAB_INSTALL_DIR/lib:$LD_LIBRARY_PATH\n'
+export LD_LIBRARY_PATH=$MOAB_INSTALL_DIR/lib:$LD_LIBRARY_PATH
