bc1ae2f2db8ba74bb964cb249547bbcb931e2dff
Samuel Stern <sstern3@wisc.edu>
2018-07-05 17:03:29 -0500

    Restructured moab and dagmc scripts to support full setup process

--- setup_scripts/dagmc_setup.sh
+++ setup_scripts/dagmc_setup.sh
@@ -10,14 +10,17 @@ cd DAGMC
 git checkout develop
 cd ..
 mkdir bld
+
 DAGMC_INSTALL_DIR=$INSTALL_ROOT/dagmc/install
+MOAB_CONFIG_DIR=$INSTALL_ROOT/MOAB/install/lib/cmake/MOAB
 mkdir $DAGMC_INSTALL_DIR
 cd bld
-cmake ../DAGMC -DCMAKE_INSTALL_PREFIX=$DAGMC_INSTALL_DIR
+cmake ../DAGMC -DCMAKE_INSTALL_PREFIX=$DAGMC_INSTALL_DIR -DMOAB_CMAKE_CONFIG=$MOAB_CONFIG_DIR
 make 
 make install
-printf '\nConsider adding the following lines to .bashrc:\n'
-printf 'export PATH=$DAGMC_INSTALL_DIR/bin:$PATH\n'
-export PATH=$DAGMC_INSTALL_DIR/bin:$PATH
-printf 'export LD_LIBRARY_PATH=$DAGMC_INSTALL_DIR/lib:$LD_LIBRARY_PATH\n'
-export LD_LIBRARY_PATH=$DAGMC_INSTALL_DIR/lib:$LD_LIBRARY_PATH
+
+printf '\nConsider running the following lines and adding them to .bashrc:\n'
+printf "export PATH=$DAGMC_INSTALL_DIR/bin"':$PATH\n'
+printf "export LD_LIBRARY_PATH=$DAGMC_INSTALL_DIR/lib"':$LD_LIBRARY_PATH\n'
+
+cd $INSTALL_ROOT
