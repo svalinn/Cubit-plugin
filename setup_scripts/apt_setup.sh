3ba4f5bfb7f39bbf94e6c666f2c3671fe23c6c9a
Lucas John Jacobson <ljacobson64@gmail.com>
2019-08-02 17:13:03 -0500

    Remove setup scripts

--- setup_scripts/apt_setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#Install all simplest-level dependencies of DAGMC-Trelis that can be obtained via apt
-apt install -y libeigen3-dev libtool make mpich libhdf5-dev cmake libarmadillo-dev
