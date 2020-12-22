f1e19b876bf5ac2fd049e22acf99fc2c6e73d69b
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2020-12-22 15:54:46 -0600

    non interactive mode

--- script/build_plugin_macos.sh
+++ script/build_plugin_macos.sh
@@ -9,8 +9,8 @@ export MANPATH=/opt/local/share/man:$MANPATH
 export LD_LIBRARY_PATH=/opt/local/lib:$LD_LIBRARY_PATH
 
 
-sudo port selfupdate
-sudo port install libtool eigen3 hdf5 cmake gcc6 wget realpath
+sudo port selfupdate -N
+sudo port install -N libtool eigen3 hdf5 cmake gcc6 wget realpath
 
 #wget https://github.com/fxcoudert/gfortran-for-macOS/releases/download/10.2/gfortran-10.2-Catalina.dmg
 #hdiutil attach gfortran-10.2-Catalina.dmg
