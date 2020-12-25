92294eff41666b937f4eebc7eecfab59984a8733
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2020-12-25 13:49:18 -0600

    patching cubitcmake

--- script/build_plugin_macos.sh
+++ script/build_plugin_macos.sh
@@ -91,6 +91,7 @@ cd ${PLUGIN_ABS_PATH}/Trelis-plugin
 git submodule update --init
 
 cd ${PLUGIN_ABS_PATH}/Trelis-plugin
+cp script/CubitExport-release.cmake ${TRELIS_INSTALL_LOC}/MacOS/
 rm -rf bld
 mkdir -pv bld
 cd bld
