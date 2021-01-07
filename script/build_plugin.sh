cc15fa97d5403f59cb105f13212d92b44a5e702b
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2021-01-06 20:14:27 -0600

    working

--- script/build_plugin.sh
+++ script/build_plugin.sh
@@ -3,20 +3,20 @@ CURRENT=$(pwd)
 SCRIPTPATH=`dirname $(dirname $(realpath $0))`
 
 PLUGIN_DIR="plugin-build"
-PLUGIN_ABS_PATH=${CURRENT}/${PLUGIN_DIR}
+PLUGIN_ABS_PATH=""
 
 source ${SCRIPTPATH}/script/linux_share_build.sh
 
 install_prerequise
-setup_Trelis_sdk
 
 setup_folder
 
 build_moab
 build_dagmc
 
-build_plugin
-build_plugin_pkg
+setup_Trelis_sdk $1
+build_plugin $1
+build_plugin_pkg $1
 
 
 
--- script/linux_share_build.sh
+++ script/linux_share_build.sh
@@ -6,21 +6,23 @@ function install_prerequise() {
     TZ=America/Chicago
     ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
     apt-get update -y
-    apt-get install -y g++ autogen autoconf libtool libeigen3-dev libhdf5-dev patchelf gfortran git cmake
+    apt-get install -y g++ libtool libeigen3-dev libhdf5-dev patchelf git cmake
 }
 
 function setup_folder() {
-
+    cd ${CURRENT}
     mkdir ${PLUGIN_DIR}
     echo "Building the Trelis plugin in ${CURRENT}\\${PLUGIN_DIR}"
 
     unset LD_LIBRARY_PATH
 
-    cd ${PLUGIN_ABS_PATH}
+    cd ${PLUGIN_DIR}
+    PLUGIN_ABS_PATH=$(pwd)
     ln -s $SCRIPTPATH/ ./
 }
 
 function build_moab() {
+    cd ${PLUGIN_ABS_PATH}
     mkdir -pv moab/bld
     cd moab
     git clone https://bitbucket.org/fathomteam/moab -b Version5.1.0
@@ -28,18 +30,20 @@ function build_moab() {
     cmake ../moab -DENABLE_HDF5=ON \
             -DCMAKE_PREFIX_PATH=/usr/lib/x86_64-linux-gnu/hdf5/serial \
             -DBUILD_SHARED_LIBS=ON \
-            -DENABLE_BLASLAPACK=OFF -DENABLE_FORTRAN=OFF -DCMAKE_CXX_FLAGS=-D_GLIBCXX_USE_CXX11_ABI=0 \
+            -DENABLE_BLASLAPACK=OFF \
+            -DENABLE_FORTRAN=OFF \
+            -DCMAKE_CXX_FLAGS=-D_GLIBCXX_USE_CXX11_ABI=0 \
             -DCMAKE_INSTALL_PREFIX=${PLUGIN_ABS_PATH}/moab
 
     make -j`grep -c processor /proc/cpuinfo`
     make install
     cd ../..
     rm -rf moab/moab moab/bld
-    cd ..
 }
 
 
 function build_dagmc(){
+    cd ${PLUGIN_ABS_PATH}
     mkdir -pv DAGMC/bld
     cd DAGMC
     git clone https://github.com/bam241/DAGMC -b build_exe
@@ -63,16 +67,17 @@ function build_dagmc(){
 }
 
 function setup_Trelis_sdk() {
-cd /Trelis-sdk 
-dpkg -i Trelis-$1-Lin64.deb
+    cd /Trelis-sdk 
+    dpkg -i Trelis-$1-Lin64.deb
 
-cd /opt
-tar -xzvf /Trelis-sdk/Trelis-SDK-$1-Lin64.tar.gz
-cd /opt/Trelis-16.5
-tar -xzvf /Trelis-sdk/Trelis-SDK-$1-Lin64.tar.gz
+    cd /opt
+    tar -xzvf /Trelis-sdk/Trelis-SDK-$1-Lin64.tar.gz
+    cd /opt/Trelis-16.5
+    tar -xzvf /Trelis-sdk/Trelis-SDK-$1-Lin64.tar.gz
 }
 
 function build_plugin(){
+    cd ${PLUGIN_ABS_PATH}
     cd Trelis-plugin
     git submodule update --init
     cd ../
@@ -87,6 +92,7 @@ function build_plugin(){
 }
 
 function build_plugin_pkg(){
+    cd ${PLUGIN_ABS_PATH}
     mkdir -p pack/bin/plugins/svalinn
     cd pack/bin/plugins/svalinn
 
