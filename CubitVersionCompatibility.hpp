18889f8d0a288085f29dc31758fa8a08da1da4ba
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2021-02-15 17:06:38 -0600

    cleanup

--- CubitVersionCompatibility.hpp
+++ CubitVersionCompatibility.hpp
@@ -5,11 +5,9 @@
 
 
 #if CUBIT_VERSION_MAJOR <= 17
-    //#include "CubitInterface.hpp"
     #define CUBIT_INTERFACE_HEADER "CubitInterface.hpp"
     #define MSG_HANDLER CubitInterface::get_cubit_message_handler()
 #else
-    //#include "CubitCoreformInterface.hpp"
     #define CUBIT_INTERFACE_HEADER "CubitCoreformInterface.hpp"
     #define MSG_HANDLER CubitMessage::get_message_handler()
 #endif
