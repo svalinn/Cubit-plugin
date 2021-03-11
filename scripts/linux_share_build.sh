c0e9f5c2756cf38dc60d96000299c13da324f3e2
Baptiste Mouginot <15145274+bam241@users.noreply.github.com>
2021-03-11 08:05:14 -0600

    Update scripts/linux_share_build.sh
    Co-authored-by: Patrick Shriwise <pshriwise@gmail.com>

--- scripts/linux_share_build.sh
+++ scripts/linux_share_build.sh
@@ -103,7 +103,7 @@ function build_dagmc(){
 function setup_Trelis_sdk() {
 
     cd ${FOLDER_PKG}
-    $SUDO apt install -y ./${TRELIS_PKG}
+    $SUDO apt-get install -y ./${TRELIS_PKG}
     cd /opt
     $SUDO tar -xzvf ${FOLDER_PKG}/${TRELIS_SDK_PKG}
     # removing app_loger that seems to not be present in Cubit 2020.2
