211b2c02d8ea7e9bfb8dab87dfcc0a12dd9d5c10
Baptiste Mouginot <15145274+bam241@users.noreply.github.com>
2021-03-15 10:45:20 -0500

    Apply suggestions from code review
    Co-authored-by: Patrick Shriwise <pshriwise@gmail.com>

--- scripts/build_plugin_docker_ubuntu.sh
+++ scripts/build_plugin_docker_ubuntu.sh
@@ -6,7 +6,7 @@
 # $2: Trelis/Cubit version to install, 17.1.0 or 2020.2
 # $3 path to local pkg folder containing both trelis/cubit install package and sdk
 # $4 name of the Trelis deb package
-# $5 name fo the Trelis sdk tarball
+# $5 name of the Trelis sdk tarball
 
 SCRIPTPATH=`dirname $(dirname $(realpath $0))`
 docker run -v "$SCRIPTPATH:/Trelis-plugin" -v "$3:/trelis-sdk" -it $1 bash -c "/Trelis-plugin/scripts/build_plugin_linux.sh $2 /trelis-sdk $4 $5; bash"
