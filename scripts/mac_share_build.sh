3445c08b41e5777838ae843bef74d1c14b2452eb
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2021-03-22 11:42:09 -0600

    verbose untar

--- scripts/mac_share_build.sh
+++ scripts/mac_share_build.sh
@@ -130,7 +130,7 @@ function setup_trelis_sdk() {
         CUBIT_BASE_NAME="Trelis-17.1"
     fi
     ls -al
-    sudo tar -xzf ${FOLDER_PKG}/${TRELIS_SDK_PKG}
+    sudo tar -xzvf ${FOLDER_PKG}/${TRELIS_SDK_PKG}
     echo "ARG 1: ${1}"
     echo "CUBIT_BASE_NAME: ${CUBIT_BASE_NAME}"
     sudo mv ${CUBIT_BASE_NAME}/* ./
