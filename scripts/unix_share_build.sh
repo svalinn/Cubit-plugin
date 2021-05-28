cbbaceb7c3f2a9f56da2cb0b166523965f90bd84
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2021-05-28 15:21:28 -0500

    any better ?

--- scripts/unix_share_build.sh
+++ scripts/unix_share_build.sh
@@ -225,7 +225,7 @@ function build_plugin(){
     mkdir -pv bld
     cd bld
     ls ${CUBIT_PATH}/MacOS/
-    cmake ../Trelis-plugin -DCMAKE_PREFIX_PATH=${CUBIT_PATH} -DCUBIT_ROOT=${CUBIT_PATH} \
+    cmake ../Trelis-plugin -DCMAKE_PREFIX_PATH=/Applications -DCUBIT_ROOT=${CUBIT_PATH} \
                            -DDAGMC_DIR=${PLUGIN_ABS_PATH}/DAGMC \
                            -DCMAKE_BUILD_TYPE=Release \
                             $CMAKE_ADDITIONAL_FLAGS \
