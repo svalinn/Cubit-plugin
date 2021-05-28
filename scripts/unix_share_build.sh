b5ef0f5c6e534d30a9992c2c6c955b0884e1b8df
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2021-05-28 12:42:32 -0500

    better?

--- scripts/unix_share_build.sh
+++ scripts/unix_share_build.sh
@@ -174,7 +174,8 @@ function mac_setup_cubit_sdk() {
 
     #  $SUDO cp -pv ${CUBIT_PATH}/MacOS/CubitGeomConfig.cmake ${CUBIT_PATH}/MacOS/CubitGeomConfig.cmake.orig
     #  $SUDO $SED -i "s/\${_IMPORT_PREFIX}\/include/\${_IMPORT_PREFIX}\/${CUBIT_BASE_NAME}.app\/include/" ${CUBIT_PATH}/MacOS/CubitGeomConfig.cmake
-        ln -s ls ${CUBIT_PATH}/bin ${CUBIT_PATH}/MacOS
+        ls ${CUBIT_PATH}/MacOS
+        ln -s ${CUBIT_PATH}/MacOS ${CUBIT_PATH}/bin
 
         #sudo ln -s ${CUBIT_PATH}/include /Applications/include
         echo "/Applications"
