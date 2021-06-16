bf2d68f62fd3c049a7ff7d3c0b793fc0535c97a1
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2021-06-16 08:58:01 -0500

    would that work in tmp ?

--- scripts/unix_share_build.sh
+++ scripts/unix_share_build.sh
@@ -263,7 +263,7 @@ function linux_build_plugin_pkg(){
     cd ../..
     tar --sort=name -czvf svalinn-plugin_${OS}_cubit_$1.tgz bin
     chmod 666 svalinn-plugin_${OS}_cubit_$1.tgz
-    cp svalinn-plugin_${OS}_cubit_$1.tgz /
+    cp svalinn-plugin_${OS}_cubit_$1.tgz /tmp
 }
 
 function mac_build_plugin_pkg(){
